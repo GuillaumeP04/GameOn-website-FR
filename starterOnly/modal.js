6d8a52c099d49837c2b8dffca320f5f624cd9828
Guillaume Payet <guillaumepayet04@gmail.com>
2021-05-24 07:56:59 +0400

    Second rough draft Issue #2

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -3,95 +3,183 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeModal = document.getElementsByClassName("close")[0];
-
-
+let form = document.querySelector('form');
+let firstName = document.getElementById('first');
+let lastName = document.getElementById('last');
+let email = document.getElementById('email');
+let birthdate = document.getElementById('birthdate');
+let numPart = document.querySelector("input[type=number]");
+let submitBtn = document.getElementById('btn-submit');
+let locationCheckbox = document.querySelectorAll("input[name=location]");
+let conditionsCheckbox = document.querySelectorAll("input[name='conditions']");
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// close modal form
+closeModal.onclick = function() {
+  modalbg.style.display = "none";
+};
+
+// submit button
+  // submitBtn.disabled = true;
+  // submitBtn.style.opacity = '0.5';
+  // submitBtn.style.cursor = 'not-allowed';
+
+// form validation listener
+form.addEventListener('submit', submitForm);
+
+firstName.addEventListener('input', firstNameValidation);
+
+lastName.addEventListener('input', lastNameValidation);
+
+email.addEventListener('input', emailValidation);
+  
+birthdate.addEventListener('input', birthdateValidation);
+
+numPart.addEventListener('input', numPartValidation);
+
+// locationCheckbox.addEventListener('change', locationValidation);
+
+for (let i = 0 ; i < locationCheckbox.length; i++) {
+  locationCheckbox[i].addEventListener('change' , locationValidation) ; 
+}
+
+for (let i = 0 ; i < conditionsCheckbox.length; i++) {
+  conditionsCheckbox[i].addEventListener('change' , conditionsValidation) ; 
+}
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-// close modal form
-closeModal.onclick = function() {
-  modalbg.style.display = "none";
+// form validation
+function firstNameValidation () {
+  let valid = false;
+  let firstName = document.getElementById('first');
+  let parent = firstName.closest('div');
+  if (firstName.value.length < 2) {
+    showError(parent);
+  } else if (firstName.value.length > 15) {
+    showError(parent);
+  } else {
+    hideError(parent);
+    valid = true;
+  }
+  return valid;
 }
 
-window.onclick = function(event) {
-  if (event.target == modalbg) {
-    modalbg.style.display = "none";
+function lastNameValidation () {
+  let valid = false;
+  let lastName = document.getElementById('last');
+  let parent = lastName.closest('div');
+  if (lastName.value.length < 2) {
+    showError(parent);
+  } else {
+    hideError(parent);
+    valid = true;
   }
-} 
-
-// validation modal form
-let firstName = document.getElementById('first');
-let lastName = document.getElementById('last');
-let email = document.getElementById('email');
-let birthdate = document.getElementById('birthdate');
-let numPart = document.getElementById('quantity');
-
-firstName.addEventListener('input', function(){
-  console.log(firstName.value)
-})
-
-lastName.addEventListener('input', function(){
-  console.log(lastName.value)
-})
-
-email.addEventListener('input', function(){
-  console.log(email.value)
-})
-
-birthdate.addEventListener('input', function(){
-  console.log(birthdate.value)
-})
-
-numPart.addEventListener('input', function(){
-  console.log(numPart.value)
-})
-
-
-// functions
-function validate() {
-      
-  if( document.reserve.firstName.value == "" || document.reserve.firstName.length < 3) {
-     alert( "Please provide your first name!" );
-     document.reserve.firstName.focus() ;
-     return false;
-  }
-  if( document.reserve.lastName.value == "" ) {
-     alert( "Please provide your last name!" );
-     document.reserve.lastName.focus() ;
-     return false;
-  }
-  if( document.reserve.email.value == "" ) {
-     alert( "Please provide your Email!" );
-     document.reserve.email.focus() ;
-     return false;
-  }
-  if( document.reserve.birthdate.value == "" ) {
-     alert( "Please provide your birthdate!" );
-     return false;
-  }
-  if( document.reserve.numPart.value == "" ) {
-     alert( "Please provide a number" );
-     return false;
-  }
-  return( true );
+  return valid;
 }
 
-function validateEmail() {
-  var emailID = document.myForm.EMail.value;
-  atpos = emailID.indexOf("@");
-  dotpos = emailID.lastIndexOf(".");
+function emailValidation () {
+  let valid = false;
+  let email = document.getElementById('email');
+  let parent = email.closest('div');
+  if (email.value.indexOf('@') == -1) {
+    showError(parent);
+  } 
+  if (email.value.indexOf('.') == -1) {
+    showError(parent);
+  }
+  if ((email.value.indexOf("@") != -1) && (email.value.indexOf(".") != -1)) {
+    hideError(parent);
+    valid = true;
+  }
+  return valid;
+}
+
+function birthdateValidation () {
+  let valid = false;
+  let birthdate = document.getElementById('birthdate');
+  let parent = birthdate.closest('div');
+  if (birthdate.value.length < 8) {
+    showError(parent);
+  } else {
+    hideError(parent);
+    valid = true;
+  }
+  return valid;
+}
+
+function numPartValidation () {
+  let valid = false;
+  let numPart = document.querySelector("input[type=number]");
+  let parent = numPart.closest('div');
+  if (numPart.value == '') {
+    showError(parent);
+  } else {
+    hideError(parent);
+    valid = true;
+  }
+  return valid;
+}
+
+function locationValidation () {
+  let valid = false;
+  let locationCheckbox = document.querySelectorAll("input[name=location]");
+  let parent = locationCheckbox.closest('div');
+  if (!locationCheckbox.checked) {
+    showError(parent);
+  } else {
+    hideError(parent);
+    valid = true;
+  }
+  return valid;
+}
+
+function conditionsValidation () {
+  let valid = false;
+  let conditionsCheckbox = document.querySelector("input[name=conditions]");
+  let parent = conditionsCheckbox.closest('div');
+  showError(parent);
+  if (!conditionsCheckbox.checked) {
+    showError(parent);
+  } else {
+    hideError(parent);
+    valid = true;
+  }
+  return valid;
+}
+
+function submitForm() {
+  submitBtn.disabled = true;
+  submitBtn.style.opacity = '0.5';
+  submitBtn.style.cursor = 'not-allowed';
+  let isFirstNameValid = firstNameValidation(),
+      isLastNameValid = lastNameValidation(),
+      isEmailValid = emailValidation(),
+      isBirthdateValid = birthdateValidation(),
+      isNumPartValid = numPartValidation(),
+      isLocationValid = locationValidation(),
+      isConditionsValid = conditionsValidation();
+  let isFormValid = isFirstNameValid && 
+  isLastNameValid && isEmailValid &&
+  isBirthdateValid && isLocationValid &&
+  isNumPartValid && isConditionsValid;
   
-  if (atpos < 1 || ( dotpos - atpos < 2 )) {
-     alert("Please enter correct email ID")
-     document.myForm.EMail.focus() ;
-     return false;
+  if (isFormValid) {
+    submitBtn.disabled = false;
+    submitBtn.style.opacity = '1';
+    submitBtn.style.cursor = 'pointer';
   }
-  return( true );
+}
+
+function showError(el) {
+  el.setAttribute('data-error-visible', true)
+}
+
+function hideError(el) {
+  el.setAttribute('data-error-visible', false)
 }
 
 function editNav() {
